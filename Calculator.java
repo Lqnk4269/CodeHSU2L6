88ac5e6644ec857646bd695f77e7313b64e2e835
Lqnk4269 <95261280+Lqnk4269@users.noreply.github.com>
2022-10-07 08:58:10 -0700

    Did the Thing

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,8 @@
+import org.jetbrains.annotations.NotNull;
+
 public class Calculator {
 
-    public static double calcAverage(double... nums) {
+    public static double calcAverage(double @NotNull ... nums) {
         double sum = 0;
         for(double x : nums) {
             sum += x;
--- CalculatorTester.java
+++ CalculatorTester.java
@@ -1,6 +1,6 @@
 public class CalculatorTester {
     public static void main(String[] args) {
         System.out.println(Calculator.calcAverage(1,2));
-        System.out.println(Calculator.calcAverage(new double[] {5.0, 6.2, 7.5})); // Reduntant
+        System.out.println(Calculator.calcAverage(new double[] {5.0, 6.2, 7.5})); // Redundant
     }
 }
